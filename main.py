5f6fa7c58ae01dcbaeb24da0a673c8a8afcd6a5a
Avery <avery.stehr@gmail.com>
2022-03-17 20:51:22 -0400

    humble beginnings

--- main.py
+++ main.py
@@ -1,12 +1,54 @@
 import pyautogui as pg
+import time 
+ # Default Settings
+pg.PAUSE = 0.5
+width, height = pg.size() 
+x = width/2
+y = height/2
+time.sleep(2)
 
-pg.PAUSE = 2.5
+def GTRI():
+    ## Personal details
+    # scroll to bottom, select image (continue)
+    pg.leftClick(x,y)
+    pg.scroll(-10000) # DOWN
+    handleImage('images/continue.png',5,10)
 
-def button():
-    pass
+    ## EU
+    handleImage('images/eu.png',25,50)
+    pg.scroll(-10000)
+    handleImage('images/continue.png',5,10)
 
-def scroll():
-    pg.scroll()
+    ## Job QUesiotnaire
+    # Yes - drop down none
+    # No
+    # continue
 
-def type():
-    pass
+    ## Documents
+    # most recent
+    # cont.
+
+    ## Education
+    # scroll to bottom 
+    # cont.
+
+    ## Ethniciy
+    # male 
+    # hispanics
+    # cont.
+
+    ## Vet
+    # Scroll, not prot vet. cont. 
+
+
+    # DIsability
+    # scroll , no , cont.
+
+    ## FInal
+    # dropdown, career website , submit
+def handleImage(image,xOffset,yOffset):
+    time.sleep(1)
+    (l, t, w, h) = pg.locateOnScreen(image)
+    pg.leftClick(l+xOffset,t+yOffset)
+
+GTRI()
